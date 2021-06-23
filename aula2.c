48e7fd5bc7afeea4f8295c18411bdada1404b175
TauganyCorrea <>
2021-06-23 18:44:46 +0000

    Initial commit

---
+++ aula2.c
@@ -0,0 +1,10 @@
+/*int main(void) {
+  float a,dolar,euro;
+printf ("digite o valor da passagem para calcular\n");
+  scanf ("%f",&a);
+  dolar = 5.05;
+  euro = 6.12;
+  printf ("%.2f passagem em dolar\n",a/dolar);
+  printf ("%.2f passagem em euro\n",a/euro);
+  return 0;
+}*/
---
+++ aula3.c
@@ -0,0 +1,115 @@
+#include "stdio.h"
+int main(void) {
+//exercicio 1
+/*
+printf("*************************\n");
+printf("*                       *\n");
+printf("*                       *\n");
+printf("*************************\n");
+//exercicio 2
+printf("-------\n");
+printf("--0-0--\n");
+printf("---0---\n");
+printf("-------\n");
+printf("-------\n");
+printf("-------\n");
+printf("-------\n");
+printf("-------\n");
+printf("---0---\n");
+printf("--0-0--\n");
+printf("--000--\n");
+printf("--0-0--\n");
+printf("-------\n");
+printf("-------\n");
+printf("-------\n");
+printf("-0--0--\n");
+printf("0-0-0--\n");
+printf("0-0-0--\n");
+printf("-0--0--\n");
+printf("-------\n");
+printf("-------\n");
+printf("-0-0-0-\n");
+printf("0-0-0-0\n");
+printf("-0-0-0-\n");
+printf("0-0-0-0\n");
+printf("-0-0-0-\n");
+printf("0-0-0-0\n");
+printf("-0-0-0-\n");
+printf("-000000\n");
+printf("------0\n");
+printf("00000-0\n");
+printf("0---0-0\n");
+printf("0-000-0\n");
+printf("0-----0\n");
+printf("0000000\n");
+//exercicio 3
+int a;
+scanf("%i",&a);
+if(a<0)
+  printf("NEGATIVO");
+  else
+  printf("POSITIVO");
+  //exercicio 4
+  int a;
+scanf("%i",&a);
+if(a==0)
+  printf("nulo");
+  if(a>0)
+  printf("POSITIVO");
+  if(a<0)
+   printf("NEGATIVO");
+   //exercicio 5
+ int a,b;
+scanf("%i%i",&a,&b);
+if(a<b)
+printf("%i",b);
+else
+printf("%i",a);
+//exercicio 6
+ int a,b;
+scanf("%i%i",&a,&b);
+if(a<b)
+printf("%i",b);
+if(a==b)
+printf("IGUAIS");
+if(a>b)
+printf("%i",a);
+//exercicio 7
+int a,b;
+scanf("%i%i",&a,&b);
+if(a<b)
+printf("%i %i",a,b);
+else
+printf("%i %i",b,a);
+//exercicio 8
+int a,b,r;
+scanf("%i%i",&a,&b);
+r= a+b;
+printf("%i+%i=%i\n",a,b,r);
+r= a-b;
+printf("%i-%i=%i\n",a,b,r);
+r= a*b;
+printf("%i*%i=%i\n",a,b,r);
+//exercicio 9
+int a,b,r;
+scanf("%i%i",&a,&b);
+r= a%b;
+if(r==0)
+printf("PAR\n");
+else
+printf("IMPAR\n");*/
+ //exercicio 10
+float n1,n2,n3,r;
+printf("digite a nota 1\n");
+scanf("%f",&n1);
+printf("digite a nota 2\n");
+scanf("%f",&n2);
+printf("digite a nota 3\n");
+scanf("%f",&n3);
+r= (n1+n2+n3)/3;
+if(r>=6.0)
+printf("aprovado\n");
+else
+printf("reprovado\n");
+ return 0;
+}
